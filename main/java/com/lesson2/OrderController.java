d22a39a55745ab9474f1eb6ff9f944d01d967f10
Nikolay <pervakmv@elas.kiev.ua>
2019-06-06 16:13:30 +0300

    Java based config робоча базова конфігурація

--- main/java/com/lesson2/OrderController.java
+++ main/java/com/lesson2/OrderController.java
@@ -1,20 +1,29 @@
 package com.lesson2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class OrderController {
 
-    @Autowired /* зависимость с контейнера достать и использовать*/
+   @Autowired /* зависимость с контейнера достать и использовать*/
     private OrderService orderService; /*Зависимость или Dependencie*/
 
     public Order save(Order order){
         return orderService.save(order);
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, value="/test", produces = "text/plain")
+    public @ResponseBody
+    String test2(){
+        System.out.println("Test 2");
+        return "Test2";
     }
 
     @RequestMapping(method = RequestMethod.GET, value="/orderSave", produces = "text/plain")
@@ -25,6 +34,4 @@ public class OrderController {
 
         return "ok";
     }
-
-
 }
--- main/java/com/lesson2/SpringConfig.java
+++
@@ -1,8 +0,0 @@
-package com.lesson2;
-
-import org.springframework.stereotype.Component;
-
-
-public class SpringConfig {
-
-}
